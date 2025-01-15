ed3a4ed6898b02e81f175a596cb3f458c3126063
anushka-devcode <anushkap393@gmail.com>
2025-01-15 09:37:47 +0530

    14 Jan 2025

--- prime_no.py
+++ prime_no.py
@@ -1,1 +1,11 @@
-n=int(input(""))
+n=int(input("enter the no:"))
+if(n>1):
+    for i in range(2,(n**0.5)+1):
+        if(n%i==0):
+            print(n,"is not a prime no>>")
+        else:
+            print(n,"is a prime no.")
+else:
+    print(n,"is not a prime no>>")
+    
+    
