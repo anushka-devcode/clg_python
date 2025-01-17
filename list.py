ff435ad52cf8fd37e9c1ff024c184178ce435603
anushka-devcode <anushkap393@gmail.com>
2025-01-17 08:43:56 +0530

    17 Jan 2025

---
+++ list.py
@@ -0,0 +1,10 @@
+marks=[90,25,78,65,55,59] #list
+print(marks[2]) #print list
+list=["Snigdha",19,16824]
+print(list[0])
+print(type(list)) #type of list
+print(type(marks))
+print(len(list)) #length of list
+print(len(marks))
+list[0]="Anushka" #Mutation of list
+print(list)
