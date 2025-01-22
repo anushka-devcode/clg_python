0cfe715bf2fa74147ebf8dabffae8fe02427a7be
anushka-devcode <anushkap393@gmail.com>
2025-01-22 09:37:24 +0530

    22 Jan 2025

---
+++ print.py
@@ -0,0 +1,37 @@
+print(10)
+print("hello world")
+print("hello\n","world")
+a="hello"
+b=10
+c=True
+print(type(b)) #class <int>
+#operators
+'''operand and operator
+operator:-  
+Arithmatic operator  + - / * % ** //
+Logical operator  and or not 
+Relational operator   < > <= >= != ==
+Assignment operator  = += -= /= *= %= 
+
+user se input chahiye- use input function
+ex=a=int(input("Enter yr no.:")) {for integer}
+{for string} a=(input("Enter yr string:"))
+
+String= collection of character
+str="Python"
+str[1]
+print(len(str)) {print no of characters using len()}
+
+slicing
+str[1:6:2]
+str[1:6]
+
+conditional statement:-
+if():
+    statement1
+elif:
+    statement2    
+else:
+    statement3
+    
+'''
