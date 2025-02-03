6917c7bc6c75e374bccf5da6f2fd4712c0f0c6d5
anushka-devcode <anushkap393@gmail.com>
2025-02-03 12:47:37 +0530

    Python Program

--- prime_no.py
+++ prime_no.py
@@ -1,6 +1,6 @@
 n=int(input("enter the no:"))
 if(n>1):
-    for i in range(2,(n**0.5)+1):
+    for i in range(2,int(n**0.5)+1):
         if(n%i==0):
             print(n,"is not a prime no>>")
         else:
